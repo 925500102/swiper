892fc192328b4c2abab6b5c68e7d1109a71a6c55
minwe <minwe@yunshipei.com>
2015-06-19 12:02:28 +0800

    v0.0.3: fixes npm deps

--- dist/amazeui.swiper.js
+++ dist/amazeui.swiper.js
@@ -1,4 +1,4 @@
-/*! amazeui-swiper v0.0.2 | by Amaze UI Team | (c) 2015 AllMobilize, Inc. | Licensed under MIT | 2015-06-19T11:54:10+0800 */ 
+/*! amazeui-swiper v0.0.3 | by Amaze UI Team | (c) 2015 AllMobilize, Inc. | Licensed under MIT | 2015-06-19T12:01:25+0800 */ 
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
 /*! Amaze UI Plugin ~ swiper */
